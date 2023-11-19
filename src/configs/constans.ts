d3206916632b724f95c8bf86dedb69b18ab9be1d
BogdanBabinczuk <b.babinczukdesign@gmail.com>
2023-11-19 18:58:56 -0500

    cambio de puerto para el backend

--- src/configs/constans.ts
+++ src/configs/constans.ts
@@ -1,1 +1,1 @@
-export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:4000/api" : "https://locallizador-backend.vercel.app/api";
+export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:3100/api" : "https://locallizador-backend.vercel.app/api";
