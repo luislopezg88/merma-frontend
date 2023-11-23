f7c055c6f186fb0b218693eea96e931c5d05c56f
LisPEx <lismarygoyo@gmail.com>
2023-11-23 17:30:13 -0500

    cambio de url vercel

--- src/configs/constans.ts
+++ src/configs/constans.ts
@@ -1,1 +1,1 @@
-export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:3100/api" : "https://locallizador-backend.vercel.app/api";
+export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:3100/api" : "https://merma-backend.vercel.app/api";
